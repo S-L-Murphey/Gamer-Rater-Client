12382f41b019b40fe886301a8a4500e1c2f525fa
S-L-Murphey <smurphey1990@gmail.com>
2021-08-06 11:31:27 -0500

    Got some auth and nav boilerplate added

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item">
+                <Link to="/">Home</Link>
+            </li>
+            <li className="navbar__item">
+                <Link to="/games">Games</Link>
+            </li>
+            <li className="navbar__item">
+            <Link to="/events">Events</Link>
+            </li>
+            {
+                (localStorage.getItem("lu_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("lu_token")
+                                props.history.push({ pathname: "/" })
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
